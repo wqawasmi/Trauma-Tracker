e3f224a838c4faace5fb64271ac420f6c36d1541
Nicholas Grieco <nrg38@scarletmail.rutgers.edu>
2017-04-23 02:23:07 -0400

    added twilio sms

--- send_sms.py
+++ send_sms.py
@@ -1,13 +1,19 @@
 import os
+import sys
 from twilio.rest import Client
 
-def send_sms(data, userPhone):
-	account_sid = "ACf5228c8f7c3e6ae961f98604a717380e"
-	auth_token  = "4ef51d16456da350c5def9506dc71f56"
+account_sid = "ACf5228c8f7c3e6ae961f98604a717380e"
+auth_token  = "4ef51d16456da350c5def9506dc71f56"
 
-	client = Client(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
-	message = client.messages.create(
-		to="+1" + userPhone, 
-		from_="+18622197472",
-		body=data)
+if(arglen(sys.argv) != 3):
+	print "Error"
+	sys.exit()
+
+data = sys.argv[1]
+userPhone = sys.argv[2]
+message = client.messages.create(
+	to="+1" + userPhone, 
+	from_="+18622197472",
+	body=data)
