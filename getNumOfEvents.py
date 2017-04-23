1e299e72451207466ebfdc7e86bf64147fa1e241
Nicholas Grieco <nrg38@scarletmail.rutgers.edu>
2017-04-23 09:38:22 -0400

    num of events

---
+++ getNumOfEvents.py
@@ -0,0 +1,16 @@
+from pymongo import *
+import os 
+import sys
+
+fileName = sys.argv[1] #file where data is written
+
+file = open(fileName, 'w')
+
+client = MongoClient('localhost', 27017)
+db = client.accelDatabase
+
+dbLength = db.accelDatabase.count()
+
+for data in db.accelDatabase.find().skip(dbLength -1):
+			print data['eventNum']
+			file.write(str(data['eventNum']))
